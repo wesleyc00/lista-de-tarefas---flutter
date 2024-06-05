770d4f9be9f0dff1f9b7c7940adf7dbcc0b0b382
Wesley Corrêa <wesleyeng00@gmail.com>
2024-06-04 23:31:10 -0300

    Alterações no widget teste e nome do app

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class TarefaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TelaTarefa(),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:projet01/main.dart';
+import 'package:lista_de_tarefas/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
